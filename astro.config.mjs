16af8372ce2ba72721700664d6fdf70a901d2007
Gerard Matos <gerardmatos1990@gmail.com>
2026-07-07 14:26:57 -0500

    feat: create a carousel component

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,20 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                '@': new URL('./src', import.meta.url).pathname,
+                '@pages': new URL('./src/pages', import.meta.url).pathname,
+                '@styles': new URL('./src/styles', import.meta.url).pathname,
+                '@layouts': new URL('./src/layouts', import.meta.url).pathname,
+                '@components': new URL('./src/components', import.meta.url)
+                    .pathname,
+                '@interfaces': new URL('./src/interfaces', import.meta.url)
+                    .pathname,
+                '@svg': new URL('./src/svg', import.meta.url).pathname,
+                '@assets': new URL('./src/assets', import.meta.url).pathname,
+                '@utils': new URL('./src/utils', import.meta.url).pathname,
+            },
+        },
     },
 })
---
+++ src/interfaces/carousel.ts
@@ -0,0 +1,9 @@
+export interface Flower {
+    id: number
+    name: string
+    image: string
+    description: string
+    price: string
+    width: number
+    height: number
+}
